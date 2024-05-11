7bebde6d6d5a9af8e3ff54378695e3140bb7b334
Kaizhi Xuan <xuankaizhi@semixchina.com>
2024-05-11 17:46:31 +0800

    1

--- dev/old/drawing/alternate/SkiaSharpOpenGLBenchmark/Form1.Designer.cs
+++ dev/old/drawing/alternate/SkiaSharpOpenGLBenchmark/Form1.Designer.cs
@@ -39,9 +39,9 @@ namespace SkiaSharpOpenGLBenchmark
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 11);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.Size = new System.Drawing.Size(39, 21);
             this.button1.TabIndex = 0;
             this.button1.Text = "10";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +49,9 @@ namespace SkiaSharpOpenGLBenchmark
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(57, 12);
+            this.button2.Location = new System.Drawing.Point(57, 11);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.Size = new System.Drawing.Size(39, 21);
             this.button2.TabIndex = 1;
             this.button2.Text = "1k";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,9 +59,9 @@ namespace SkiaSharpOpenGLBenchmark
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(102, 12);
+            this.button3.Location = new System.Drawing.Point(102, 11);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(39, 23);
+            this.button3.Size = new System.Drawing.Size(39, 21);
             this.button3.TabIndex = 2;
             this.button3.Text = "10k";
             this.button3.UseVisualStyleBackColor = true;
@@ -69,9 +69,9 @@ namespace SkiaSharpOpenGLBenchmark
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(147, 12);
+            this.button4.Location = new System.Drawing.Point(147, 11);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(39, 23);
+            this.button4.Size = new System.Drawing.Size(39, 21);
             this.button4.TabIndex = 3;
             this.button4.Text = "100k";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,18 +80,21 @@ namespace SkiaSharpOpenGLBenchmark
             // skglControl1
             // 
             this.skglControl1.BackColor = System.Drawing.Color.Purple;
-            this.skglControl1.Location = new System.Drawing.Point(12, 41);
+            this.skglControl1.Location = new System.Drawing.Point(12, 38);
             this.skglControl1.Name = "skglControl1";
-            this.skglControl1.Size = new System.Drawing.Size(600, 400);
+            this.skglControl1.Size = new System.Drawing.Size(600, 369);
             this.skglControl1.TabIndex = 4;
             this.skglControl1.VSync = false;
             this.skglControl1.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs>(this.skglControl1_PaintSurface);
+            this.skglControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.skglControl1_MouseDown);
+            this.skglControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.skglControl1_MouseMove);
+            this.skglControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.skglControl1_MouseUp);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 451);
+            this.ClientSize = new System.Drawing.Size(624, 416);
             this.Controls.Add(this.skglControl1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
--- dev/old/drawing/alternate/SkiaSharpOpenGLBenchmark/Form1.cs
+++ dev/old/drawing/alternate/SkiaSharpOpenGLBenchmark/Form1.cs
@@ -10,44 +10,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SkiaSharpOpenGLBenchmark
 {
     public partial class Form1 : Form
     {
+        private static SKMatrix viewMatrix = SKMatrix.MakeIdentity();
+        private Point lastPoint;
         public Form1()
         {
             InitializeComponent();
+            skglControl1.MouseWheel += SkglControl1_MouseWheel;
+        }
+
+        private void SkglControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // 获取滚轮滚动的增量
+            float delta = e.Delta;
+
+            // 计算缩放因子
+            float scaleFactor = delta > 0 ? 1.1f : 0.9f;
+
+            // 获取当前鼠标位置
+            Point point = e.Location;
+
+            // 更新视图矩阵
+            var makeScale = SKMatrix.MakeScale(scaleFactor, scaleFactor, (float)point.X, (float)point.Y);
+            SKMatrix.PostConcat(ref viewMatrix, makeScale);
+            // 重新绘制
+
+            skglControl1.Invalidate();
         }
 
         Random rand = new Random(0);
+
         private void skglControl1_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
         {
-            var surface = e.Surface;
-            surface.Canvas.Clear(SKColor.Parse("#003366"));
-            for (int i = 0; i < lineCount; i++)
+            var canvas = e.Surface.Canvas;
+            // 应用视图矩阵
+            canvas.Concat(ref viewMatrix);
+
+            // 芯片中间的间隔
+            const int dX = 15;
+            const int dY = 15;
+
+            // 视野显示宽度高度
+            const int viewShowWidthPx = 30;
+            const int viewShowHeightPx = 30;
+            // 创建绘制参数和矩形对象
+            var paint = new SKPaint { Color = SKColors.LightBlue };
+            // 开始绘制
+            canvas.Clear(SKColors.White); // 清空画布
+
+            for (var row = 0; row < lineCount; row++)
             {
-                var paint = new SKPaint
+                for (var col = 0; col < lineCount; col++)
                 {
-                    Color = new SKColor(
-                        red: (byte)rand.Next(255),
-                        green: (byte)rand.Next(255),
-                        blue: (byte)rand.Next(255),
-                        alpha: (byte)rand.Next(255)),
-                    StrokeWidth = rand.Next(1, 10),
-                    IsAntialias = true
-                };
-                surface.Canvas.DrawLine(
-                    x0: rand.Next(skglControl1.Width),
-                    y0: rand.Next(skglControl1.Height),
-                    x1: rand.Next(skglControl1.Width),
-                    y1: rand.Next(skglControl1.Height),
-                    paint: paint);
+                    // 计算矩形的坐标
+                    var xView = col * viewShowWidthPx;
+                    var yView = row * viewShowHeightPx;
+                    var xCell1 = xView + dX;
+                    var yCell1 = yView + dY;
+                    var xCell2 = xView + viewShowWidthPx;
+                    var yCell2 = yView + viewShowHeightPx;
+                    var rect = new SKRect
+                    {
+                        // 设置矩形的位置
+                        Left = xCell1,
+                        Top = yCell1,
+                        Right = xCell2,
+                        Bottom = yCell2
+                    };
+
+
+                    // 绘制矩形
+                    canvas.DrawRect(rect, paint);
+                }
             }
         }
 
         int lineCount;
         List<double> renderTimesMsec = new List<double>();
+
         private void Benchmark(int lineCount, int times = 10)
         {
             rand = new Random(0);
@@ -64,29 +109,58 @@ namespace SkiaSharpOpenGLBenchmark
                 renderTimesMsec.Add(1000.0 * stopwatch.ElapsedTicks / Stopwatch.Frequency);
                 double mean = renderTimesMsec.Sum() / renderTimesMsec.Count();
                 Debug.WriteLine($"Render {renderTimesMsec.Count:00} " +
-                    $"took {renderTimesMsec.Last():0.000} ms " +
-                    $"(running mean: {mean:0.000} ms)");
+                                $"took {renderTimesMsec.Last():0.000} ms " +
+                                $"(running mean: {mean:0.000} ms)");
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Benchmark(10);
+            Benchmark(1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Benchmark(1_000);
+            Benchmark(10);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Benchmark(10_000);
+            Benchmark(100);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Benchmark(100_000);
+            Benchmark(1000);
+        }
+
+        private unsafe void skglControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 如果鼠标左键被按下，则进行移动操作
+            if (e.Button == MouseButtons.Left)
+            {
+                Point point = e.Location;
+                SKPoint delta = new SKPoint((float)(point.X - lastPoint.X), (float)(point.Y - lastPoint.Y));
+                var makeTranslation = SKMatrix.MakeTranslation(delta.X, delta.Y);
+                SKMatrix.PostConcat(ref viewMatrix, makeTranslation);
+
+                lastPoint = point;
+
+                skglControl1.Invalidate();
+            }
+    
+        }
+
+        private void skglControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 记录鼠标按下时的位置
+            lastPoint = e.Location;
+        }
+
+        private void skglControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 清空记录的位置
+            lastPoint = new Point();
         }
     }
 }
